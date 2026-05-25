011877ab03b43a3200b076f5ffa3d8b5b0b6053e
Withhell <watherd29@gmail.com>
2026-05-25 18:42:35 +0300

    added requirements and cleaned forecast.py

--- forecast.py
+++ forecast.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import openmeteo_requests
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
